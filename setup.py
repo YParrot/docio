28ebdad93183c15e5743b49ca6c5444d779f242d
Motoki Naruse <motoki@naru.se>
2015-10-31 01:49:40 +0900

    fix_classifiers

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython'
+        'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Text Processing',
         'Topic :: Utilities',
     ],
